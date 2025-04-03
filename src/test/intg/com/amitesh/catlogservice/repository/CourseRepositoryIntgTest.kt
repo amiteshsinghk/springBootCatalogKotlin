d677af882bb89848c27fc6135562542fcb65a74e
Amitesh Singh <amiteshsinghk@gmail.com>
2025-04-03 22:42:42 +0530

    unit test for parametrized function

--- src/test/intg/com/amitesh/catlogservice/repository/CourseRepositoryIntgTest.kt
+++ src/test/intg/com/amitesh/catlogservice/repository/CourseRepositoryIntgTest.kt
@@ -5,10 +5,16 @@ import com.amitesh.catlogservice.util.getDemoCourseList
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.test.context.ActiveProfiles
+import java.util.stream.Stream
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @DataJpaTest
 @ActiveProfiles("test")
 class CourseRepositoryIntgTest {
@@ -37,6 +43,21 @@ class CourseRepositoryIntgTest {
         val courses = courseRepository.findCoursesByName("Course2222")
         println("courses :: $courses")
         Assertions.assertEquals(1, courses.size)
+    }
+
+    @ParameterizedTest
+    @MethodSource("courseAndSize")
+    fun findCoursesByName_approach2(name: String, expectedSize: Int){
+        val courses = courseRepository.findCoursesByName(name)
+        println("courses :: $courses")
+        Assertions.assertEquals(expectedSize, courses.size)
+    }
+
+    companion object{
+        @JvmStatic
+        fun courseAndSize(): Stream<Arguments>{
+            return Stream.of(Arguments.arguments("Course2222",1))
+        }
     }
 
 }
