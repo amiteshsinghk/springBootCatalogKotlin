3871972b68c1ed4fd05239aa6ad15fe1d9d0476d
Amitesh Singh <amiteshsinghk@gmail.com>
2025-04-04 17:37:28 +0530

    optimise build.gradle dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,43 +23,30 @@ dependencies {
 	//web
 	implementation("org.springframework.boot:spring-boot-starter-web")
 
-	//web
-//	implementation("org.springframework.boot:spring-boot-starter-validation")
-
-	//kotlin
-//	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-//	implementation("org.jetbrains.kotlin:kotlin-reflect")
-//	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-
-	//validator
-//	implementation("org.springframework.boot:spring-boot-starter-validation")
-
-	//logging
-//	implementation("io.github.microutils:kotlin-logging-jvm:2.0.11")
-
-
-	//db
-//	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-//	runtimeOnly("com.h2database:h2")
-//    runtimeOnly("org.postgresql:postgresql")
-
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.springframework.boot:spring-boot-starter-webflux")
-	testImplementation("io.mockk:mockk:1.10.4")
-	testImplementation("com.ninja-squad:springmockk:3.0.1")
-
 	//logging
 	implementation("io.github.microutils:kotlin-logging-jvm:2.0.11")
 
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	//validator
 	implementation("org.springframework.boot:spring-boot-starter-validation")
+
+	//kotlin
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	runtimeOnly("com.h2database:h2")
+//	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+	//h2 in memory database
+//	runtimeOnly("com.h2database:h2")
+	//database
 	runtimeOnly("org.postgresql:postgresql")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+	testImplementation("org.springframework.boot:spring-boot-starter-webflux")
+	testImplementation("io.mockk:mockk:1.10.4")
+	testImplementation("com.ninja-squad:springmockk:3.0.1")
 }
 
 kotlin {
